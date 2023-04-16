5e3e47b2e50ea73aed4341ad4700aadd03b33649
akintunde adedoyin <akintunde_afolabi@yahoo.com>
2023-04-16 08:51:16 +0100

    change the flex property for the nav and dashboard in the NavBar component and also in the tailwind config

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -5,7 +5,7 @@ import powerIcon from '../../assets/icons/VectoractivitiesIcon.svg';
 import addIcon from '../../assets/icons/PlusIcon.svg';
 const NavBar = () => {
   return (
-    <nav className='bg-primary p-12 flex flex-col md:flex-3'>
+    <nav className='bg-primary p-12 flex flex-col flex-3'>
         {/* Nav button*/}
         <Button className='bg-onPrimary text-white py-[1.2rem] px-[1.6rem] leading-[1.9rem] '>
             <img src={powerIcon} alt="" className='w-[2.2rem] h-[2.2rem]' />
