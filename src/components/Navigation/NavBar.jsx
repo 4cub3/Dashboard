21c6252cf65f42252f479f0fddfde64220b6520c
akintunde adedoyin <akintunde_afolabi@yahoo.com>
2023-04-16 14:54:53 +0100

    adjusted the nav width

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
   return (
     <>
        <Button className='p-24 text-gray-500 absolute top-[7rem] sm:top-28 right-0 z-[1000]' onclick={()=>{setNavExpand(prev => !prev)}}><FaBars/></Button>
-    <nav className={`bg-primary absolute h-screen z-[900] smd:relative flex flex-col flex-3 overflow-hidden transition-all duration-300 ${navExpand ? 'w-auto p-12 transition-all duration-300' : 'w-0  p-0 smd:w-auto'}`}>
+    <nav className={`bg-primary absolute h-screen z-[900] smd:relative flex flex-col flex-3 overflow-hidden transition-all duration-300 ${navExpand ? 'p-12 transition-all duration-300' : 'w-0  p-0 smd:w-auto'}`}>
         {/* Nav button*/}
         <Button className='bg-onPrimary text-white py-[1.2rem] px-[1.6rem] leading-[1.9rem] '>
             <img src={powerIcon} alt="" className='w-[2.2rem] h-[2.2rem]' />
